6e7d98e2a2c8a5f00a9172ca31b9277bfc9389b7
Eliza Drinker <elizaleedrinker@gmail.com>
2022-12-01 17:58:01 -0800

    Update Part1 sol'n

--- day1/part1.js
+++ day1/part1.js
@@ -4,13 +4,15 @@ import { fileURLToPath } from 'node:url';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-try {
-  var cals = await readFile(join(__dirname, 'data.js'), { encoding: 'utf8' });
-  var maxCals = cals.split('\n\n').reduce((max, inventory) => Math.max(
-      max,
-      inventory.split('\n').reduce((total, item) => total + Number(item), 0),
-  ), 0);
-  console.log('maxCals', maxCals);
-} catch (error) {
-  console.error('there was an error:', error.message);
-}
+export default (async function() {
+  try {
+    const cals = await readFile(join(__dirname, 'data.js'), { encoding: 'utf8' });
+    const maxCals = cals.split('\n\n').reduce((max, inventory) => Math.max(
+        max,
+        inventory.split('\n').reduce((total, item) => total + Number(item), 0),
+    ), 0);
+    return maxCals;
+  } catch (error) {
+    console.error('there was an error:', error.message);
+  }
+})();
